d113563457af9d54866617b226644f99a6ebd1c6
Tianyi Pan <tianyi.pan@clobotics.com>
2023-02-12 17:45:26 +0800

    Add auto-push if --push

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+set -e
+
 git archive -v -o __app.tar.gz --format=tar.gz HEAD
 docker build -t tipani86/xiaopan-chat:latest -f Dockerfile .
+
+# If supplied with --push argument, do a docker push
+
+if [[ $1 == "--push" ]]; 
+then
+  docker push tipani86/xiaopan-chat:latest
+fi
