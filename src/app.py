9398a0eb2676846fbd58a95e3eb1d76b725b8664
Tianyi Pan <tianyi.pan@clobotics.com>
2023-02-16 10:25:52 +0800

    Add Bing tracking script in header

--- src/app.py
+++ src/app.py
@@ -93,7 +93,15 @@ st.markdown(f"""<style>
         padding-top: {padding}rem;
         padding-bottom: 0rem;
     }}
-</style>""", unsafe_allow_html=True)
+</style>
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){{
+        c[a]=c[a]||function(){{(c[a].q=c[a].q||[]).push(arguments)}};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    }})(window, document, "clarity", "script", "fuwdd48n5i");
+</script>
+""", unsafe_allow_html=True)
 
 header = st.container()
 
