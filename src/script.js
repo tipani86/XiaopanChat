e7cb5b82424d183161536abbafbaed2098d473ea
Tianyi Pan <tianyi.pan@clobotics.com>
2023-02-24 01:59:50 +0800

    Fix js bug

--- src/script.js
+++ src/script.js
@@ -20,13 +20,15 @@ const streamlitDoc = window.parent.document
 // Audio autoplay block override on mobile browsers (general idea from https://stackoverflow.com/questions/13266474/autoplay-audio-on-mobile-safari)
 
 streamlitDoc.voicePlayer = document.getElementById('voicePlayer')
-streamlitDoc.addEventListener('click', () => {
-    streamlitDoc.voicePlayer.load()
-    streamlitDoc.removeEventListener('click', () => {})
-})
+// streamlitDoc.addEventListener('click', () => {
+//     streamlitDoc.voicePlayer.load()
+//     // Remove event listener after first click
+//     streamlitDoc.removeEventListener('click', () => {})
+// })
 streamlitDoc.addEventListener('keydown', function(e) {
     if (e.key === 'Enter') {
         streamlitDoc.voicePlayer.load()
+        // Remove event listener after first keydown
         streamlitDoc.removeEventListener('keydown', function(e) {})
     }
 })
