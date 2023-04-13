cbc756ae1c75d5f284cade3c96419fd2e5719595
Tianyi Pan <tianyi.pan@clobotics.com>
2023-04-13 14:28:47 +0800

    Remove the donation message

--- src/app.py
+++ src/app.py
@@ -336,6 +336,7 @@ if "MEMORY" not in st.session_state:
 # Render footer
 with footer:
     st.info("免责声明：聊天机器人基于海量互联网文本训练的大型语言模型，仅供娱乐。小潘AI不对信息的准确性、完整性、及时性等承担任何保证或责任。", icon="ℹ️")
+    """
     st.success("小潘AI背后由OpenAI的GPT4付费模型驱动。但我们依然想给国内的用户提供无门槛服务。如果您感觉小潘AI对你带来了价值，我们非常感谢您的爱心捐赠🙏！")
     with st.expander("显示支付码"):
         left_col, right_col = st.columns(2, gap="medium")
@@ -343,6 +344,7 @@ with footer:
             st.image(os.path.join(ROOT_DIR, "src", "assets", "alipay.png"))
         with right_col:
             st.image(os.path.join(ROOT_DIR, "src", "assets", "wechat_pay.png"))
+    """
     st.markdown(f"<p style='text-align: right'><small><i><font color=gray>Build: {build_date}</font></i></small></p>", unsafe_allow_html=True)
 
 
